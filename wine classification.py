1011a0fc415b5a512427833f28d26532289c893f
Ji-InPark <81283634+Ji-InPark@users.noreply.github.com>
2024-10-30 12:48:47 +0900

    wine 구현 완료

--- wine classification.py
+++ wine classification.py
@@ -1,46 +1,58 @@
 import numpy as np
 import pandas as pd
-from sklearn import tree
+from sklearn import svm
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import classification_report, confusion_matrix
 from sklearn.model_selection import KFold
 from sklearn.model_selection import train_test_split
 
 from db_conn import *
 
+table_name = 'wine'
 
-class class_iris_classification():
+
+class class_wine_classification():
     def __init__(self):
         pass
 
-    def import_iris_data(self):
+    def import_wine_data(self):
         conn, cur = open_db()
 
-        create_sql = """ 
-            drop table if exists iris;
+        create_sql = f""" 
+            drop table if exists {table_name};
         
-            create table iris (
+            create table {table_name} (
                 id int auto_increment primary key,
-                sepal_length float,
-                sepal_width float,
-                petal_length float,
-                petal_width float,
-                species varchar(10), 
-                enter_date datetime default now() 
+                class int,
+                alcohol float,
+                malic_acid float,
+                ash float,
+                alcalinity_of_ash float,
+                magnesium int,
+                total_phenols float,
+                flavanoids float,
+                nonflavanoid_phenols float,
+                proanthocyanins float,
+                color_intensity float,
+                hue float,
+                od280_od315 float,
+                proline int
                 ); 
         """
 
         cur.execute(create_sql)
         conn.commit()
 
-        file_name = 'iris.csv'
-        iris_data = pd.read_csv(file_name)
+        file_name = 'wine.data.csv'
+        wine_data = pd.read_csv(file_name)
 
         rows = []
 
-        insert_sql = """insert into iris(sepal_length, sepal_width, petal_length, petal_width, species)
-                        values(%s,%s,%s,%s,%s);"""
+        insert_sql = f"""insert into {table_name}(class, alcohol, malic_acid, ash, alcalinity_of_ash, magnesium, total_phenols, flavanoids, nonflavanoid_phenols, proanthocyanins, color_intensity, hue, od280_od315, proline)
+                        values(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s);"""
 
-        rows = [r for r in iris_data.itertuples(index=False, name=None)]
+        rows = [r for r in wine_data.itertuples(index=False, name=None)]
 
         cur.executemany(insert_sql, rows)
         conn.commit()
@@ -51,32 +63,30 @@ class class_iris_classification():
 
         conn, cur = open_db()
 
-        sql = "select * from iris;"
+        sql = f"select * from {table_name};"
         cur.execute(sql)
 
         data = cur.fetchall()
 
-        self.X = [(t['sepal_length'], t['sepal_width'], t['petal_length'],
-                   t['petal_width']) for t in data]
-        # self.X = [ (t['sepal_length'], t['sepal_width'] ) for t in data ]
-        # self.X = [ (t['sepal_length'], t['petal_length'] ) for t in data ]
+        self.X = [(t['alcohol'], t['malic_acid'], t['ash'],
+                   t['alcalinity_of_ash'], t['magnesium'], t['total_phenols'],
+                   t['flavanoids'], t['nonflavanoid_phenols'],
+                   t['proanthocyanins'], t['color_intensity'], t['hue'],
+                   t['od280_od315'], t['proline']) for t in data]
 
         self.X = np.array(self.X)
 
-        self.y = [0 if t['species'] == 'setosa' else 1 if t[
-                                                              'species'] == 'versicolor' else 2
-                  for t in data]
+        self.y = [t['class'] for t in data]
         self.y = np.array(self.y)
 
     def data_split_train_test(self):
-
         self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(
-            self.X, self.y, test_size=0.4, random_state=None)
+            self.X, self.y, test_size=0.3, random_state=42)
 
     def classification_performance_eval_multiclass(self, y_test, y_predict,
         verbose=True):
-        target_names = ['setosa', 'versicolor', 'virginica']
-        labels = [0, 1, 2]
+        target_names = ['1', '2', '3']
+        labels = [1, 2, 3]
 
         self.confusion_matrix = confusion_matrix(y_test, y_predict,
                                                  labels=labels)
@@ -96,12 +106,12 @@ class class_iris_classification():
             print(
                 f"\n[classification_report]\n{classification_report_for_print}")
 
-    def train_and_test_dtree_model(self):
-        dtree = tree.DecisionTreeClassifier()
-        dtree_model = dtree.fit(self.X_train, self.y_train)
-        self.y_predict = dtree_model.predict(self.X_test)
+    def train_and_test_svm_model(self):
+        model = svm.SVC()
+        model.fit(self.X_train, self.y_train)
+        self.y_predict = model.predict(self.X_test)
 
-    def multiclass_dtree_KFold_performance(self):
+    def multiclass_svm_KFold_performance(self):
         kfold_reports = []
 
         kf = KFold(n_splits=5, random_state=42, shuffle=True)
@@ -114,9 +124,73 @@ class class_iris_classification():
             X_train, X_test = self.X[train_index], self.X[test_index]
             y_train, y_test = self.y[train_index], self.y[test_index]
 
-            dtree = tree.DecisionTreeClassifier()
-            dtree_model = dtree.fit(X_train, y_train)
-            y_predict = dtree_model.predict(X_test)
+            model = svm.SVC()
+            model.fit(X_train, y_train)
+            y_predict = model.predict(X_test)
+
+            # 성능 평가 후 kfold_reports에 저장
+            self.classification_performance_eval_multiclass(y_test, y_predict,
+                                                            verbose=True)
+            kfold_reports.append(
+                pd.DataFrame(self.classification_report).transpose())
+
+        # 모든 폴드의 평균 성능을 계산하고 출력
+        mean_report = pd.concat(kfold_reports).groupby(level=0).mean()
+        print(f'\n\n\n[Mean report]\n{mean_report}')
+
+    def train_and_test_logistic_regression_model(self):
+        model = LogisticRegression()
+        model.fit(self.X_train, self.y_train)
+        self.y_predict = model.predict(self.X_test)
+
+    def multiclass_logistic_regression_KFold_performance(self):
+        kfold_reports = []
+
+        kf = KFold(n_splits=5, random_state=42, shuffle=True)
+
+        i = 0
+        for train_index, test_index in kf.split(self.X):
+            i += 1
+            print(f"\n\nFold {i} :")
+
+            X_train, X_test = self.X[train_index], self.X[test_index]
+            y_train, y_test = self.y[train_index], self.y[test_index]
+
+            model = LogisticRegression()
+            model.fit(X_train, y_train)
+            y_predict = model.predict(X_test)
+
+            # 성능 평가 후 kfold_reports에 저장
+            self.classification_performance_eval_multiclass(y_test, y_predict,
+                                                            verbose=True)
+            kfold_reports.append(
+                pd.DataFrame(self.classification_report).transpose())
+
+        # 모든 폴드의 평균 성능을 계산하고 출력
+        mean_report = pd.concat(kfold_reports).groupby(level=0).mean()
+        print(f'\n\n\n[Mean report]\n{mean_report}')
+
+    def train_and_test_random_forest_model(self):
+        model = RandomForestClassifier(n_estimators=5, random_state=0)
+        model.fit(self.X_train, self.y_train)
+        self.y_predict = model.predict(self.X_test)
+
+    def multiclass_random_forest_KFold_performance(self):
+        kfold_reports = []
+
+        kf = KFold(n_splits=5, random_state=42, shuffle=True)
+
+        i = 0
+        for train_index, test_index in kf.split(self.X):
+            i += 1
+            print(f"\n\nFold {i} :")
+
+            X_train, X_test = self.X[train_index], self.X[test_index]
+            y_train, y_test = self.y[train_index], self.y[test_index]
+
+            model = RandomForestClassifier(n_estimators=5, random_state=0)
+            model.fit(X_train, y_train)
+            y_predict = model.predict(X_test)
 
             # 성능 평가 후 kfold_reports에 저장
             self.classification_performance_eval_multiclass(y_test, y_predict,
@@ -129,20 +203,66 @@ class class_iris_classification():
         print(f'\n\n\n[Mean report]\n{mean_report}')
 
 
-def multiclass_dtree_train_test_performance():
-    clf = class_iris_classification()
+def multiclass_svm_train_test_performance():
+    clf = class_wine_classification()
+    clf.import_wine_data()
     clf.load_data_for_multiclass_classification()
     clf.data_split_train_test()
-    clf.train_and_test_dtree_model()
+    clf.train_and_test_svm_model()
     clf.classification_performance_eval_multiclass(clf.y_test, clf.y_predict)
 
 
-def multiclass_dtree_KFold_performance():
-    clf = class_iris_classification()
+def multiclass_svm_KFold_performance():
+    clf = class_wine_classification()
+    clf.import_wine_data()
     clf.load_data_for_multiclass_classification()
-    clf.multiclass_dtree_KFold_performance()
+    clf.multiclass_svm_KFold_performance()
+
+
+def multiclass_logistic_regression_train_test_performance():
+    clf = class_wine_classification()
+    clf.import_wine_data()
+    clf.load_data_for_multiclass_classification()
+    clf.data_split_train_test()
+    clf.train_and_test_logistic_regression_model()
+    clf.classification_performance_eval_multiclass(clf.y_test, clf.y_predict)
+
+
+def multiclass_logistic_regression_KFold_performance():
+    clf = class_wine_classification()
+    clf.import_wine_data()
+    clf.load_data_for_multiclass_classification()
+    clf.multiclass_logistic_regression_KFold_performance()
+
+
+def multiclass_random_forest_train_test_performance():
+    clf = class_wine_classification()
+    clf.import_wine_data()
+    clf.load_data_for_multiclass_classification()
+    clf.data_split_train_test()
+    clf.train_and_test_random_forest_model()
+    clf.classification_performance_eval_multiclass(clf.y_test, clf.y_predict)
+
+
+def multiclass_random_forest_KFold_performance():
+    clf = class_wine_classification()
+    clf.import_wine_data()
+    clf.load_data_for_multiclass_classification()
+    clf.multiclass_random_forest_KFold_performance()
 
 
 if __name__ == "__main__":
-    multiclass_dtree_train_test_performance()
-    multiclass_dtree_KFold_performance()
+    print(
+        "========================================multiclass_svm_train_test_performance========================================")
+    multiclass_svm_train_test_performance()
+    multiclass_svm_KFold_performance()
+
+    print(
+        "========================================multiclass_logistic_regression_train_test_performance========================================")
+    multiclass_logistic_regression_train_test_performance()
+    multiclass_logistic_regression_KFold_performance()
+
+    print(
+        "========================================multiclass_random_forest_train_test_performance========================================")
+    multiclass_random_forest_train_test_performance()
+    multiclass_random_forest_KFold_performance()
